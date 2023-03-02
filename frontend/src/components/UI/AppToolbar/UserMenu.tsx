5c6eb50b4163f2a0321076f749861678dfde1061
Aleksey Ivanov <lepri4dw@gmail.com>
2023-03-02 15:16:53 +0600

    Initial commit, added Login and Register

---
+++ frontend/src/components/UI/AppToolbar/UserMenu.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { User } from '../../../types';
+import { Button, Menu, MenuItem } from '@mui/material';
+import { useAppDispatch } from '../../../app/hooks';
+import { logout } from '../../../features/users/usersThunks';
+
+interface Props {
+  user: User;
+}
+
+const UserMenu: React.FC<Props> = ({user}) => {
+  const dispatch = useAppDispatch();
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      <Button
+        onClick={handleClick}
+        color="inherit"
+      >
+        Hello, {user.username}
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default UserMenu;
